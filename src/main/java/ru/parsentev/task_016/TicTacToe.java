3b245a4a6d66ff6c873ab7fa0fc56ccec56a0f61
QmBo <qrioflat@gmail.com>
2018-02-26 17:56:48 +0300

    task_016 try

--- src/main/java/ru/parsentev/task_016/TicTacToe.java
+++ src/main/java/ru/parsentev/task_016/TicTacToe.java
@@ -21,6 +21,49 @@ public class TicTacToe {
     }
 
     public boolean hasWinner() {
-        throw new UnsupportedOperationException();
+        boolean isAWinner = false;
+        int[] toSend = new int[3];
+        for (int i = 0; i < 3; i++) {
+            toSend = this.values[i];
+            if (exists(toSend)) {
+                isAWinner = true;
+            }
+        }
+        for (int j = 0; j < 3; j++) {
+            for (int i = 0; i < 3; i++) {
+                toSend[i] = this.values[i][j];
+            }
+            if (exists(toSend)) {
+                isAWinner = true;
+            }
+        }
+        for (int i = 0; i < 3; i++) {
+            toSend[i] = this.values[i][i];
+        }
+        if (exists(toSend)) {
+            isAWinner = true;
+        }
+        for (int j = 2; j >= 0; j--) {
+            for (int i = 0; i < 3; i++) {
+                toSend[i] = this.values[i][j];
+            }
+        }
+        if (exists(toSend)) {
+            isAWinner = true;
+        }
+        return isAWinner;
+    }
+
+    public boolean exists(int[] expect) {
+        boolean result = false;
+        for (int validate : expect) {
+            if (validate != expect[0]) {
+                result = false;
+                break;
+            } else {
+                result = true;
+            }
+        }
+        return result;
     }
 }
