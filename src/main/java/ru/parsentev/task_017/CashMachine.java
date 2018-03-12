e3436a1013ad417fd13ba7234eff915d7694a806
qrio <qrioflat@gmail.com>
2018-03-12 22:59:30 +0300

    task_017 is Done

--- src/main/java/ru/parsentev/task_017/CashMachine.java
+++ src/main/java/ru/parsentev/task_017/CashMachine.java
@@ -5,7 +5,6 @@ import org.slf4j.Logger;
 import java.util.ArrayList;
 import java.util.List;
 
-import static java.util.Collections.singletonList;
 import static org.slf4j.LoggerFactory.getLogger;
 
 /**
@@ -24,8 +23,33 @@ public class CashMachine {
     }
 
     public List<List<Integer>> exchange(int note) {
-        List<List<Integer>> resilt = new ArrayList<List<Integer>>();
-
-        return resilt;
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        if (note == values[0]) {
+            List<Integer> list = new ArrayList<Integer>();
+            list.add(values[0]);
+            result.add(list);
+        } else {
+            for (int i = values.length - 1; i >= 0; i--) {
+                int tmp = note;
+                if (tmp >= values[i]) {
+                    int coin = tmp / values[i];
+                    List<Integer> change = new ArrayList<Integer>();
+                    for (int j = 0; j < coin; j++) {
+                        change.add(values[i]);
+                    }
+                    result.add(change);
+                    if (coin > 1 && values[i] != 1) {
+                        List<Integer> subchange = new ArrayList<Integer>();
+                        subchange.add(values[i]);
+                        coin = (tmp - values[i]) / values[i - 1];
+                        for (int j = 0; j < coin; j++) {
+                            subchange.add(values[i - 1]);
+                        }
+                        result.add(subchange);
+                    }
+                }
+            }
+        }
+        return result;
     }
 }
